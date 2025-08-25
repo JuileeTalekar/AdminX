86c35510faca9177f030672134adb4f9b9bdba0d
Juilee Talekar <juileetalekar@gmail.com>
2025-08-25 23:00:07 +0530

    create base server & client

---
+++ server/controllers/auth-controller.js
@@ -0,0 +1,47 @@
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
+
+const home = async (req, res) => {
+  try {
+    res.status(200).json({ msg: "Welcome to our home page" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    const { username, email, phone , password } = req.body;
+     const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+
+   // hashing password
+// const saltRound = 10;
+// const hashed_Password = await bcrypt.hash(password, saltRound);
+
+const userCreated = await User.create({ 
+  username,
+  email,
+  phone,
+  // password: hashed_Password
+  password
+});
+
+    // res.status(201).json({ message: "User registered successfully" });
+     res.status(201).json({
+      msg: "Registration Successful",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
+
+
+    
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { home, register };
