2a6e2a6cfe4d8d16eb42e7f8904f9a9254c9fc17
Juilee Talekar <juileetalekar@gmail.com>
2026-01-19 22:13:32 +0530

    Bug Fix

--- client/src/components/layouts/Public-Layout.jsx
+++ client/src/components/layouts/Public-Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar";
+import { Footer } from "../Footer";
 
 export const PublicLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
+    
+    
   );
 };
